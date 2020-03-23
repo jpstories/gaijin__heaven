913864e40c4af5a8caf333800134d7a1d1764d9f
David <41709736+jpstories@users.noreply.github.com>
2020-03-23 14:37:59 +1000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,12 +6,14 @@ const inputCitiesFrom = document.querySelector('.input__cities-from'),
     inputBox = document.querySelector('.input'),
     formSearch = document.querySelector('.form-search'),
     inputDate = document.querySelector('.input__date-depart'),
-    wrap = document.querySelector('.wrap');
+    wrap = document.querySelector('.wrap'),
+    cheapestTicket = document.getElementById('cheapest-ticket'),
+    otherCheapTickets = document.getElementById('other-cheap-tickets');
 
 // Тесты
-// inputCitiesFrom.value = 'Хабаровск';
-// inputCitiesTo.value = 'Токио';
-// inputDate.value = '2020-03-27';
+inputCitiesFrom.value = 'Хабаровск';
+inputCitiesTo.value = 'Токио';
+inputDate.value = '2020-03-27';
 
 //Необходимые данные для запросов на сервера
 const cityAPI = 'http://api.travelpayouts.com/data/ru/cities.json',
@@ -25,7 +27,7 @@ let citiesRU = [];
 let citiesJP = [];
 let city_RU_JP = [];
 
-//Блок для карточек
+//Старый блок для карточек
 const priceList = document.createElement('div');
 priceList.classList.add('priceList');
 wrap.append(priceList);
@@ -33,6 +35,20 @@ wrap.append(priceList);
 //Лоадер
 let loader = document.createElement('div');
 loader.classList.add('loader');
+
+//Блок, если город не найден
+let cityNotFound = document.createElement('div');
+
+const notFoundShow = (str) => {
+    let icon = '&#10006 ';
+    cityNotFound.classList.add('city_not_found');
+    cityNotFound.innerHTML = icon + str;
+    document.body.append(cityNotFound);
+}
+
+const notFoundHide = () => {
+    cityNotFound.remove();
+}
 
 //Получаем данные с сервера
 const getData = (url, callback) => {
@@ -64,7 +80,7 @@ getData(proxy + cityAPI, (data) => {
     //Соединяем 2 массива обьектов(RU, JP) в 1, для удобства
     city_RU_JP = citiesRU.concat(citiesJP);
     //Сортируем массив по алфавиту
-    const citySort = (cityArray) =>{
+    const citySort = (cityArray) => {
         cityArray.sort((a, b) => {
             if (a.name > b.name) return 1;
             if (a.name < b.name) return -1;
@@ -80,7 +96,8 @@ const showCity = (input, ul) => {
     inputBox.append(ul);
     ul.textContent = '';
 
-    const citiesFilter = city_RU_JP.filter(city => city.name.toLowerCase().includes(input.value.toLowerCase()));
+    //Ищем похожее название
+    const citiesFilter = city_RU_JP.filter(city => city.name.toLowerCase().startsWith(input.value.toLowerCase()));
 
     citiesFilter.forEach(city => {
         const li = document.createElement('li');
@@ -115,6 +132,7 @@ document.addEventListener('click', (e) => {
 formSearch.addEventListener('submit', (event) => {
     priceList.append(loader);
     event.preventDefault();
+    let calendarOption = '';
 
     // Ищем в массиве введеный город, берем оттуда код города
     const cityFrom = city_RU_JP.find((item) => {
@@ -124,39 +142,86 @@ formSearch.addEventListener('submit', (event) => {
     const cityTo = city_RU_JP.find((item) => {
         return item.name === inputCitiesTo.value
     });
-    //Формируем настройки запроса для API календарь цен
-    const calendarOption = `?depart_date=${inputDate.value}&origin=${cityFrom.code}&destination=${cityTo.code}&one_way=true`;
-
-    //Посылаем запрос на сервер, формируем url, получаем ответ
-    getData(proxy + calendar + calendarOption, (response) => {
-        //Парсим полученные данные в обьект
-        const ticketValue = JSON.parse(response);
-        const bestPrice = ticketValue.best_prices;
-        //Заходим в массив с обьектами, в каждом обьекте ищем ключ value, сортируем по значению
-        const responseSort = () =>{
-            bestPrice.sort((a, b) => {
-                if (a.value > b.value) return 1;
-                if (a.value < b.value) return -1;
-                return 0;
-            });
-        };
-        //Сортируем обьект по введенной дате, формируем карточку
-        const ticketCreate = () =>{
-            bestPrice.forEach((item) => {
-                if (item.depart_date === inputDate.value) {
-                    const spanTicket = document.createElement('span');
-                    spanTicket.innerHTML = `<div class="ticketCurrentDate">
-                                                <span>Дата отправки: ${item.depart_date}</span>
-                                                <span>Продавец: ${item.gate}</span>
-                                                <span>Цена: <mark>${item.value}</mark> руб.</span>
-                                                <button>В корзину</button>
-                                            </div>`;
-                    priceList.append(spanTicket);
-                };
-            });
-        };
-        responseSort();
-        ticketCreate();
+    //Защита
+    if (cityFrom == undefined || cityTo == undefined) {
         loader.remove();
-    });
+    } else {
+        //Формируем настройки запроса для API календарь цен
+        calendarOption = `?depart_date=${inputDate.value}&origin=${cityFrom.code}&destination=${cityTo.code}&one_way=true`;
+    }
+
+    if (cityFrom == undefined || cityTo == undefined) {
+        notFoundShow('Город не найден');
+    } else {
+        //Посылаем запрос на сервер, формируем url, получаем ответ
+        getData(proxy + calendar + calendarOption, (data) => {
+            notFoundHide();
+            //Парсим полученные данные в обьект
+            const ticketValue = JSON.parse(data);
+            const bestPrice = ticketValue.best_prices;
+            console.log(bestPrice);
+            let dateTicket;
+            //Сортировка по введенной дате
+            const sortDateTicket = () => {
+                dateTicket = bestPrice.find((item) => { return item.depart_date === inputDate.value });
+            }
+            //Сортировка по возрастанию, если много билетов
+            const sortValueTicket = () => {
+                if (dateTicket && dateTicket > 1) {
+                    let sortTicket = dateTicket.sort((a, b) => {
+                        if (a.value > b.value) return 1;
+                        if (a.value < b.value) return -1;
+                        return 0;
+                    });
+                    ticketCreate(sortTicket);
+                }
+            }
+            //Если билет существует и найден
+            const createRealTicket = () => {
+                if (dateTicket) {
+                    ticketCreate(dateTicket);
+                } else {
+                    notFoundShow('Извините, но билет на указанную дату не найден');
+                };
+            }
+            sortDateTicket();
+            sortValueTicket();
+            createRealTicket();
+            loader.remove();
+        });
+    };
+    //Заходим в массив с обьектами, в каждом обьекте ищем ключ value, сортируем по возрастанию
+    //Формируем карточку
+    const ticketCreate = (item) => {
+        priceList.innerHTML = '';
+        const spanTicket = document.createElement('article');
+        spanTicket.classList.add('ticket');
+        spanTicket.innerHTML = `
+            <h3 class="agent">${item.gate}</h3>
+            <div class="ticket__wrapper">
+                <div class="left-side">
+                    <a href="https://www.aviasales.ru/search/SVX2905KGD1" class="button button__buy">Купить
+                        за ${item.value}₽</a>
+                </div>
+                <div class="right-side">
+                    <div class="block-left">
+                        <div class="city__from">Вылет из города
+                            <span class="city__name">${item.origin}</span>
+                        </div>
+                        <div class="city__to">Город назначения:
+                            <span class="city__name">${item.destination}</span>
+                        </div>
+                        
+                    </div>
+
+                    <div class="block-right">
+                        <div class="changes">Пересадки: ${item.number_of_changes}</div>
+                        <div class="date">Вылет: ${item.depart_date}</div>
+                    </div>
+                </div>
+            </div>
+        `;
+        priceList.innerHTML = `<h2>Самый дешевый билет на выбранную дату</h2>`;
+        priceList.append(spanTicket);
+    };
 });
